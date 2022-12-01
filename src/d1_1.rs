aade4e1845d22ca48b0404e0f520d7f2f3dcdcb2
Wqrld <wereld03@gmail.com>
2022-12-01 13:14:19 +0100

    small cleanup of d1 files

--- src/d1_1.rs
+++ src/d1_1.rs
@@ -1,35 +1,30 @@
-use std::collections::{BinaryHeap, HashMap};
+use std::collections::BinaryHeap;
 use std::fs;
 
-// elve calories
-pub fn main(){
-        println!("hi!");
-        let contents = fs::read_to_string("./data/d1_1.txt").expect("Could not read file");
-        let splitted = contents.split("\r\n");
-        let mut counter = 0;
+// Day 1 - Elve calories
+pub fn main() {
+    let file = fs::read_to_string("./data/d1_1.txt").expect("Could not read file");
+    let lines = file.lines();
 
-        let mut heap = BinaryHeap::new();
-        let mut sum = 0;
-        for line in splitted {
-                if line == "" {
-                        counter += 1;
-                        heap.push(sum);
-                        sum = 0;
-
-                } else {
-                        match line.parse::<i32>() {
-                                Ok(n) =>  {sum += n}
-                                Err(e) => println!("unknown error: {}, {:?}", e, line)
-                        }
-
-                }
+    let mut heap = BinaryHeap::new();
+    let mut sum = 0;
+    for line in lines {
+        if line == "" {
+            heap.push(sum);
+            sum = 0;
+        } else {
+            match line.parse::<i32>() {
+                Ok(n) => sum += n,
+                Err(e) => println!("Parsing error: {}, {:?}", e, line)
+            }
         }
+    }
 
 
-        let mut total = 0;
-        total += heap.pop().unwrap_or_default();
-        println!("The total for the #1 is: {}", total);
-        total += heap.pop().unwrap_or_default();
-        total += heap.pop().unwrap_or_default();
-        println!("the total for the top3 is: {}", total)
+    let mut total = 0;
+    total += heap.pop().unwrap_or_default();
+    println!("The total for the #1 is: {}", total);
+    total += heap.pop().unwrap_or_default();
+    total += heap.pop().unwrap_or_default();
+    println!("the total for the top3 is: {}", total)
 }
